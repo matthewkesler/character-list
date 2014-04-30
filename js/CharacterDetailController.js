5c6d28defee7952153421449ec27e455fcf8a4fa
Matt Kesler <mkesler@captechconsulting.com>
2014-04-30 11:18:25 -0400

    remove some stuff

--- js/CharacterDetailController.js
+++ js/CharacterDetailController.js
@@ -1,3 +1,4 @@
 angular.module('CharacterList').controller('CharacterDetailController', function($scope, character) {
+    window.scrollTo(0,0);
     $scope.character = character;
 });
--- js/CharacterListController.js
+++ js/CharacterListController.js
@@ -1,4 +1,3 @@
 angular.module('CharacterList').controller('CharacterListController', function($scope, characters) {
     $scope.characters = characters;
-    $scope.characterFilter = null;
 });
